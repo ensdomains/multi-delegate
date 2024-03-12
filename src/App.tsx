d87d16af8480a572f37983cdaab286f7de711b2d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-03-11 23:40:42 -0400

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 
-import './style.css'
-import { Home } from './screens/Home'
 import { Layout } from './components/Layout'
 import { Nav } from './components/Nav'
+import { Home } from './screens/Home'
+import './style.css'
 
 function App() {
   return (
