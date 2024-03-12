ed816ea6bf7ae2f6784b680913e29a8fa5515bc7
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-03-11 23:32:02 -0400

    Start layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+import '@rainbow-me/rainbowkit/styles.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { ThemeProvider } from 'styled-components'
 import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
 import { WagmiProvider } from 'wagmi'
@@ -16,10 +18,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={lightTheme}>
-            <ThorinGlobalStyles />
-            <App />
-          </ThemeProvider>
+          <RainbowKitProvider>
+            <ThemeProvider theme={lightTheme}>
+              <ThorinGlobalStyles />
+              <App />
+            </ThemeProvider>
+          </RainbowKitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </BrowserRouter>
