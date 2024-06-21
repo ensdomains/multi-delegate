a310a6287b0ecf9eaa51032f890c7dd44c278dac
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-06-21 16:24:12 -0400

    Update env defaults

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -3,4 +3,8 @@ export const GRAPH_URL =
   'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
 
 export const RPC_URL =
-  import.meta.env.VITE_PUBLIC_RPC_URL || 'https://cloudflare-eth.com'
+  import.meta.env.VITE_PUBLIC_RPC_URL ||
+  'https://virtual.mainnet.rpc.tenderly.co/78d3d569-cb63-45a9-8b8c-9d152d90c3ed'
+
+export const PONDER_URL =
+  import.meta.env.VITE_PUBLIC_PONDER_URL || 'http://localhost:42069'
