d0cf777fa1bb4914c3c443c394fca2f47318add3
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-06-21 20:26:30 -0400

    Show original delegate in share page

--- apps/web/src/screens/SharedStrategy.tsx
+++ apps/web/src/screens/SharedStrategy.tsx
@@ -1,13 +1,15 @@
 import { Card, Heading, Helper, Spinner, Typography } from '@ensdomains/thorin'
 import { useParams } from 'react-router-dom'
+import { ensTokenContract } from 'shared/contracts'
 import { Address, isAddress } from 'viem'
-import { useEnsAddress, useEnsName } from 'wagmi'
+import { useEnsAddress, useEnsName, useReadContracts } from 'wagmi'
 
 import { DelegatePill } from '../components/DelegatePill'
 import { InnerCard } from '../components/InnerCard'
 import { useDelegates } from '../hooks/useDelegates'
 import { truncateAddress } from '../lib/utils'
 
+/* TODO: Maybe break some of this file into a separate component since it shares quite a bit of code with <Strategy /> */
 export function SharedStrategy() {
   const { addressOrName } = useParams() as { addressOrName: string }
   const isParamAddress = isAddress(addressOrName, { strict: false })
@@ -30,6 +32,25 @@ export function SharedStrategy() {
 
   const multiDelegate = useDelegates(address)
 
+  const { data: delegateInfo } = useReadContracts({
+    contracts: [
+      {
+        ...ensTokenContract,
+        functionName: 'delegates',
+        args: address ? [address] : undefined,
+      },
+      {
+        ...ensTokenContract,
+        functionName: 'balanceOf',
+        args: address ? [address] : undefined,
+      },
+    ],
+  })
+
+  const [_delegateFromTokenContract, _balance] = delegateInfo || []
+  const balance = _balance?.result
+  const delegateFromTokenContract = _delegateFromTokenContract?.result
+
   if (ensName.isLoading || ensAddress.isLoading) {
     return <Spinner size="medium" />
   }
@@ -49,15 +70,25 @@ export function SharedStrategy() {
           will not be shown here.
         </Typography>
 
-        {/* TODO: Break this out into a separate component since it shares logic with <Strategy /> */}
         <InnerCard className="flex flex-wrap justify-center gap-2">
-          {/* Delegations from multi-delegate contract */}
-          {multiDelegate.data?.map((delegate) => (
-            <DelegatePill
-              address={delegate.delegate}
-              amount={BigInt(delegate.amount)}
-            />
-          ))}
+          <>
+            {/* Delegation from token contract */}
+            {balance && (
+              <DelegatePill
+                address={delegateFromTokenContract}
+                amount={balance}
+                tooltip="From the token contract"
+              />
+            )}
+
+            {/* Delegations from multi-delegate contract */}
+            {multiDelegate.data?.map((delegate) => (
+              <DelegatePill
+                address={delegate.delegate}
+                amount={BigInt(delegate.amount)}
+              />
+            ))}
+          </>
         </InnerCard>
       </Card>
     </>
