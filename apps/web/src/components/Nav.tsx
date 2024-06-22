25a3465367c8174fe5dd9512b8630a25c60d0813
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-06-22 16:19:42 -0400

    Add callout for testing, fix copy

--- apps/web/src/components/Nav.tsx
+++ apps/web/src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Profile, Typography } from '@ensdomains/thorin'
+import { Helper, Profile, Typography } from '@ensdomains/thorin'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 import { Link } from 'react-router-dom'
 import { useAccount, useDisconnect } from 'wagmi'
@@ -12,46 +12,53 @@ export function Nav() {
   const { disconnect } = useDisconnect()
 
   return (
-    <nav className="flex justify-between gap-4">
-      <div className="flex items-center gap-6">
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+    <>
+      <Helper type="warning" alignment="horizontal">
+        Use this JSON RPC in your wallet:
+        https://virtual.mainnet.rpc.tenderly.co/78d3d569-cb63-45a9-8b8c-9d152d90c3ed
+      </Helper>
 
-        <div className="flex gap-4">
-          <Link to="https://ensdao.org/">
-            <Typography asProp="span" color="grey" weight="bold">
-              DAO
-            </Typography>
+      <nav className="flex justify-between gap-4">
+        <div className="flex items-center gap-6">
+          <Link to="/">
+            <img src={logo} alt="logo" />
           </Link>
-          <Link to="https://app.ens.domains/">
-            <Typography asProp="span" color="grey" weight="bold">
-              App
-            </Typography>
-          </Link>
+
+          <div className="flex gap-4">
+            <Link to="https://ensdao.org/">
+              <Typography asProp="span" color="grey" weight="bold">
+                DAO
+              </Typography>
+            </Link>
+            <Link to="https://app.ens.domains/">
+              <Typography asProp="span" color="grey" weight="bold">
+                App
+              </Typography>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      {address ? (
-        <Profile
-          size="medium"
-          address={address}
-          onClick={openConnectModal}
-          dropdownItems={[
-            {
-              label: 'Disconnect',
-              onClick: () => disconnect(),
-              color: 'red',
-            },
-          ]}
-        />
-      ) : (
-        <img
-          className="transition hover:-translate-y-[1px] hover:opacity-75"
-          src={profileIcon}
-          onClick={openConnectModal}
-        />
-      )}
-    </nav>
+        {address ? (
+          <Profile
+            size="medium"
+            address={address}
+            onClick={openConnectModal}
+            dropdownItems={[
+              {
+                label: 'Disconnect',
+                onClick: () => disconnect(),
+                color: 'red',
+              },
+            ]}
+          />
+        ) : (
+          <img
+            className="transition hover:-translate-y-[1px] hover:opacity-75"
+            src={profileIcon}
+            onClick={openConnectModal}
+          />
+        )}
+      </nav>
+    </>
   )
 }
