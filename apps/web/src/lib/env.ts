27ab189f01a07502d93a9c929c22191305e350a0
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-03-26 16:09:08 -0400

    Stop being annoying and go back to longer env vars

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -1,11 +1,11 @@
 export const GRAPH_URL =
-  import.meta.env.VITE_GRAPH_URL ||
+  import.meta.env.VITE_PUBLIC_GRAPH_URL ||
   'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
 
 export const RPC_URL =
-  import.meta.env.VITE_RPC_URL || 'https://ethereum-rpc.publicnode.com'
+  import.meta.env.VITE_PUBLIC_RPC_URL || 'https://ethereum-rpc.publicnode.com'
 
-export const PONDER_URL = import.meta.env.VITE_PONDER_URL
+export const PONDER_URL = import.meta.env.VITE_PUBLIC_PONDER_URL
 
 export const REOWN_PROJECT_ID =
-  (import.meta.env.VITE_REOWN_PROJECT_ID as string) || ''
+  (import.meta.env.VITE_PUBLIC_REOWN_PROJECT_ID as string) || ''
